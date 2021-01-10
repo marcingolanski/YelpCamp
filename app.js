3613df44ffbee0756f7d54ef78d3064f78182691
Marcin Golanski <marcelgola80@gmail.com>
2021-01-10 20:53:34 +0000

    add mongolab

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ app.get(function(req, res, next){
 });
 
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect("mongodb://localhost/Yelp_Camp_v12", { useNewUrlParser: true } );
+//mongodb://localhost/Yelp_Camp_v12
+mongoose.connect("mongodb+srv://marcel-gola:kubusX3456@yelpcamp.3dakb.mongodb.net/test", { useNewUrlParser: true } );
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
